0d95256cafe7cc814132b1e76836e90809b27b54
Jay <dunnpoker98@gmail.com>
2023-10-24 04:00:56 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/promptdefi-web',
+  base: '/ai',
   plugins: [
     react(),
     nodePolyfills({
