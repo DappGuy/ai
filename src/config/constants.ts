ebc1059e56f8b32dda5deb449945222c2c9c5cce
Jay <dunnpoker98@gmail.com>
2023-10-25 00:14:18 +1000

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = import.meta.env.VITE_APP_TITLE;
 
-export const BUNDLER_URL = 'https://bundler-hackathon.madfish.xyz/rpc';
+export const BUNDLER_URL = 'https://goerli-bundler.etherspot.io/';
 export const PROMT_API_URL = 'https://k5xcxsb7-3000.aue.devtunnels.ms/parse-prompt';
 export const ETHERSCAN_TX_URL = 'https://goerli.etherscan.io/tx';
 export const LINK_FAUCET_URL = 'https://faucets.chain.link/goerli';
