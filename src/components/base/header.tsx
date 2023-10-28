0f9db48d80138df0a6be73e845061a0f0bc58b54
Jay <dunnpoker98@gmail.com>
2023-10-29 00:17:54 +1000

    Update header.tsx

--- src/components/base/header.tsx
+++ src/components/base/header.tsx
@@ -1,18 +1,36 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, IconButton, Toolbar, Typography, Menu, MenuItem } from '@mui/material';
 import { InfoPanel } from './info-panel/info-panel';
 import { ChangeMode } from '../ui/change-mode';
 import { useIsDesktop } from '@/hooks/is-desktop';
 
 export const Header = () => {
   const { isDesktop } = useIsDesktop();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null); // Define the type for anchorEl
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (event && event.currentTarget) {
+      setAnchorEl(event.currentTarget);
+    }
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuItems = [
+    { label: 'Item 1', onClick: handleMenuClose },
+    { label: 'Item 2', onClick: handleMenuClose },
+    { label: 'Item 3', onClick: handleMenuClose },
+  ];
 
   return (
     <AppBar
       position="static"
       sx={{
-        backgroundImage: 'url("https://dreamcanvas.xyz/bg.jpg")', // specify the background image
+        backgroundImage: 'url("https://dreamcanvas.xyz/bg.jpg")',
         color: 'header.text',
-        height: 'shape.headerHeight'
+        height: 'var(--shape-headerHeight)' // Ensure --shape-headerHeight is defined
       }}
     >
       <Toolbar
@@ -33,7 +51,36 @@ export const Header = () => {
           variant="h6"
           component="div"
         ></Typography>
-        <ChangeMode />
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleMenuOpen}
+          sx={{ mr: 2 }}
+        >
+          <i className="material-icons">menu</i>
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          {menuItems.map((item, index) => (
+            <MenuItem key={index} onClick={item.onClick}>
+              {item.label}
+            </MenuItem>
+          ))}
+        </Menu>
+        <ChangeMode /> {/* Assuming ChangeMode is a valid component */}
         {!isDesktop && <InfoPanel />}
       </Toolbar>
     </AppBar>
