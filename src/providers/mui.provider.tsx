264d46cdb4ce0633f6b7837c2d463931110b793a
Jay <dunnpoker98@gmail.com>
2023-10-29 20:41:11 +1000

    Update mui.provider.tsx

--- src/providers/mui.provider.tsx
+++ src/providers/mui.provider.tsx
@@ -1,76 +1,80 @@
 import { useMemo } from 'react';
-
-import { CssBaseline, PaletteMode, PaletteOptions, ThemeProvider, createTheme } from '@mui/material';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import { indigo, blueGrey } from '@mui/material/colors';
-
 import { ColorMode, useColorMode } from './mode.provider';
-
 import { OverrideLinkBehaviorThemeComponents } from '@/components/base';
 import { CFC } from '@/types/react';
+import { LinkProps } from '@mui/material/Link';
 
 const common = {
-  primary: indigo
+  primary: indigo,
 };
 
-const pallettes: Record<PaletteMode, PaletteOptions> = {
+const pallettes = {
   [ColorMode.LIGHT]: {
     mode: ColorMode.LIGHT,
     background: {
       default: '#F5F5F5',
-      paper: '#fff'
+      paper: '#fff',
     },
     header: {
       background: indigo[500],
-      text: '#fff'
+      text: '#fff',
     },
-    ...common
+    ...common,
   },
   [ColorMode.DARK]: {
     mode: ColorMode.DARK,
     background: {
       default: '#121212',
-      paper: '#1e1e1e'
+      paper: '#1e1e1e',
     },
     header: {
       background: indigo[900],
-      text: '#fff'
+      text: '#fff',
     },
-    ...common
-  }
+    ...common,
+  },
 };
-
 
 export const MuiProvider: CFC = ({ children }) => {
   const { mode } = useColorMode();
 
-  const theme = useMemo(
-    () => {
-      return createTheme({
-        palette: pallettes[mode],
-        components: {
-          ...OverrideLinkBehaviorThemeComponents,
-          MuiCssBaseline: {
-            styleOverrides: {
-              body: {
-                backgroundColor: mode === ColorMode.DARK ? blueGrey[900] : blueGrey[100],
-              },
-            },
-          },
-          MuiContainer: {
-            styleOverrides: {
-              root: {
-                backgroundColor: 'rgb(55, 65, 81)', // Set the background color for all Container components
-              },
+  const theme = useMemo(() => {
+    return createTheme({
+      palette: pallettes[mode],
+      components: {
+        ...OverrideLinkBehaviorThemeComponents,
+        MuiCssBaseline: {
+          styleOverrides: {
+            body: {
+              backgroundColor: mode === ColorMode.DARK ? blueGrey[900] : blueGrey[100],
             },
           },
         },
-        shape: {
-          headerHeight: '4rem',
+        MuiGrid: {
+          styleOverrides: {
+            root: {
+              backgroundColor: 'rgb(55, 65, 81)', // Set the background color for all Grid components
+            },
+          },
         },
-      });
-    },
-    [mode]
-  );
+        MuiContainer: {
+          styleOverrides: {
+            root: {
+              backgroundColor: 'rgb(55, 65, 81)', // Set the background color for all Container components
+            },
+          },
+        },
+        MuiButtonBase: {
+          // ... custom configurations for MuiButtonBase
+        },
+      },
+      shape: {
+        headerHeight: '4rem',
+      },
+    });
+  }, [mode]);
 
   return (
     <ThemeProvider theme={theme}>
