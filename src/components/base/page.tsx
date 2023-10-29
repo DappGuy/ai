9c576202b6b4563e8495baccd12bd1bfc01a8ea6
Jay <dunnpoker98@gmail.com>
2023-10-29 22:03:07 +1000

    Update page.tsx

--- src/components/base/page.tsx
+++ src/components/base/page.tsx
@@ -3,8 +3,9 @@ import { Box, styled } from '@mui/material';
 export const Page = styled(Box)(({ theme }) => ({
   paddingLeft: theme.spacing(1),
   paddingRight: theme.spacing(1),
-
   display: 'flex',
   flexDirection: 'column',
-  flexGrow: 1
+  flexGrow: 1,
+  backgroundColor: 'rgb(55, 65, 81)' // Add the background color here
 }));
+
