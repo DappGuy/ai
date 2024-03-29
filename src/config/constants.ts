9806144faecce692c492d4e781324733c4ce7d61
Jay <dunnpoker98@gmail.com>
2024-03-30 02:29:45 +1000

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = import.meta.env.VITE_APP_TITLE;
 
 export const BUNDLER_URL = 'https://mainnet-bundler.etherspot.io/';
-export const PROMT_API_URL = 'https://k5xcxsb7-3000.aue.devtunnels.ms/parse-prompt';
+export const PROMT_API_URL = 'https://gq5gpmn6-3000.aue.devtunnels.ms/parse-prompt';
 export const ETHERSCAN_TX_URL = 'https://mainnet.etherscan.io/tx';
 export const LINK_FAUCET_URL = 'https://faucets.chain.link/mainnet';
 
